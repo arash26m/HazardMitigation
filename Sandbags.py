1e706ea8cd2f0b0094607df2e9d55f30458f7715
Arash Taghinezhad <54901882+arash26m@users.noreply.github.com>
2021-07-08 11:55:28 -0700

    Update Sandbags.py

--- Sandbags.py
+++ Sandbags.py
@@ -1,11 +1,11 @@
-print('Sandbags Calculator helps you calculate the number of sangbags you need to protect your home from flood')
+print('Sandbags Calculator helps you to calculate the number of sangbags you need to protect your home from flood')
 L=int(input('• How many linear feet of protection on ground do you need to protect your home with sandbags? '))
 H=int(input('• What is the projected flood height in your area? '))
 SL=15
 SW=11
 SH=3
 NL = (L*12+4*SL)/SL
-NH = (H*12+4*SH)/SH
+NH = (H*12+1*SH)/SH
 print(NL)
 print(NH)
 print('Total number of sandbags you need = ', round(NL*NH))
